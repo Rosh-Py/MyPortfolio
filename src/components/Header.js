544726ece1098697f2470281f11b6431e58aceeb
Rosh-Py <jharoshan.k54@gmail.com>
2021-06-26 16:14:37 +0530

    fixed page flickering issue

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ import { navLinks, pageTitle, typeSpeed } from "../data";
 const Header = () => {
   const { openSidebar, changeTheme, isThemeChanged } = useGlobalContext();
   const [title, setTitle] = useState("");
-  window.scrollTo(0, 0);
+
   //typewriter effect starts
   let i = 0;
   const typeWriter = () => {
@@ -39,7 +39,8 @@ const Header = () => {
       if (window.scrollY > navbarOffset && !window.isNavbarFixed) {
         navbar.classList.add("navbar-fixed");
         window.isNavbarFixed = true;
-        window.offsetValue = window.scrollY;
+        //Checking for minimum offsetvalue
+        window.offsetValue = Math.min(navbarOffset, window.scrollY); //Added to avoid flickering issue
       } else if (window.scrollY < window.offsetValue && window.isNavbarFixed) {
         navbar.classList.remove("navbar-fixed");
         window.isNavbarFixed = false;
